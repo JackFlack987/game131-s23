bcfc0f73a0d9122a6570e492f6af43a4d2e392a9
Jack Flack <jackflack987@gmail.com>
2023-02-19 10:38:26 -0600

    Project1

--- Assets/LoadSettingsFromFile.cs
+++ Assets/LoadSettingsFromFile.cs
@@ -5,28 +5,47 @@ using UnityEngine;
 public class LoadSettingsFromFile : MonoBehaviour
 {
 
-    // Set in the inspector
     public BounceController bounceController;
-    public TextAsset file;
+  // Set in the inspector
+  public BounceController bounceController;
+  public TextAsset file;
 
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
+  {
+    string textContents = file.text;
+    string[] lines = textContents.Split(new char[] { '\n', '\r' }, System.StringSplitOptions.RemoveEmptyEntries);
+
+    for (int i = 0; i < lines.Length; i++)
     {
-        string textContents = file.text;
-        string[] lines = textContents.Split(new char[] { '\n', '\r'}, System.StringSplitOptions.RemoveEmptyEntries);
+      // TODO: Parse the line into values.
+      // Assign those values to the appropriate fields in bounceController.
+     
+      string line = lines[i];
+      string[] words = line.Split(' ');
 
-        for (int i = 0; i < lines.Length; i++) 
-        {
-            // TODO: Parse the line into values.
-            // Assign those values to the appropriate fields in bounceController.
-        }
-
+      string color = words[0];
+     
+      if (color == "red")
+      {
+       bounceController.redSpawns = int.Parse(words[1]);
+       bounceController.redSpawnRate = float.Parse(words[2]);
+       bounceController.redHealth = int.Parse(words[3]);
+      }
+      if (color == "blue")
+      {
+        bounceController.blueSpawns = int.Parse(words[1]);
+        bounceController.blueSpawnRate = float.Parse(words[2]);
+        bounceController.blueHealth = int.Parse(words[3]);
+      }
 
     }
+    
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
 }
